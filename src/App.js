e58a754ba685bddc61f0af615e7a936704a8d063
Shiki <3211314336@qq.com>
2021-12-24 09:49:47 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import { Button } from "antd";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Button type="primary">Primary Button</Button>
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
