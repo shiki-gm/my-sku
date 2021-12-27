45fc89a170e363c3189bfda8bb629b95379f616e
Shiki <3211314336@qq.com>
2021-12-27 16:28:16 +0800

    u

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ export default function App() {
       data.goodsAttributeValueList.length > 0 &&
         data.goodsAttributeValueList.forEach((data1) => {
           tempData[index][data1.name] = {
+            rootDefault: data1.stockNum === 0,
             default: data1.stockNum === 0,
             value: false
           };
@@ -53,7 +54,7 @@ export default function App() {
               ])}
               onClick={() => onClick(item)}
             >
-              {data[item]["default"] && (
+              {(data[item]["default"] || data[item]["rootDefault"]) && (
                 <div
                   className={classnames([
                     "tips",
@@ -94,7 +95,7 @@ export default function App() {
       }
     }
 
-    setBtntype(tempData);
+    console.log("tempSelectedTag", tempSelectedTag);
 
     // 1.isCancel是true时，表示取消按钮选中，则应清空相关其他按钮点选状态
     // 2.isCancel是false时，表示选中，则应关联相关其他阿牛的点选状态
@@ -107,27 +108,53 @@ export default function App() {
     //
 
     let selectedStr = Object.values(tempSelectedTag).join("+");
+    let selectedArr = Object.values(tempSelectedTag);
     // 黑色+小码
     const tempSelect = {};
     for (let item in formatSku) {
-      // item: 黑色+小码+老版
+      console.log("item", item);
+      // item: 黑色+小码+老版 ， 这里没有考虑跳着选的情况
       const filter1 = item.indexOf(selectedStr) > -1;
+      tempSelect[item] = formatSku[item];
+
+      const keyArr = item.split("+");
+      console.log("keyArr", keyArr);
       if (filter1) {
-        tempSelect[item] = formatSku[item];
+        // 如果进入已选列表，则标记
+        // 过滤如果数量尾0，则继续标记售罄
+        if (formatSku[item] === 0) {
+          keyArr.forEach((keyName, index) => {
+            tempData[index][keyName]["default"] = true;
+          });
+        }
+      } else {
+        selectedArr.forEach((item, index) => {
+          // 其他进入选出列表，取消标记
+          if (!keyArr.includes(item)) {
+            tempData[index][item]["default"] = false;
+          }
+        });
       }
     }
-    setSelectedSku(tempSelect);
-    const tempSelectKeys = Object.keys(tempSelect);
 
-    for (let keyName in tempSelectKeys) {
-      console.log("keyName", tempSelectKeys[keyName]);
-      const aaa = tempSelectKeys[keyName].split("+");
-      console.log(aaa);
-      // keyName.replace(/\+?(\w+)\+?/g, (match, p1) => {
-      //   console.log(match, p1);
-      //   return 1;
-      // });
-    }
+    setBtntype(tempData);
+    // 对已选中的组合进行过滤，缩小遍历范围
+    setSelectedSku(tempSelect);
+
+    // const tempSelectKeys = Object.keys(tempSelect);
+
+    // for (let keyName in tempSelectKeys) {
+    //   console.log("keyName", tempSelectKeys[keyName]);
+    //   const keyArr = tempSelectKeys[keyName].split("+");
+    //   console.log(keyArr);
+
+    //   if (tempSelect[keyName] === 0) {
+    //     //过滤如果数量尾0，则继续标记售罄
+    //     for (let item in keyArr) {
+
+    //     }
+    //   }
+    // }
 
     // const tempFormatSku = JSON.parse(JSON.stringify(formatSku));
     // if (selectedSku.length === 0) {
@@ -150,12 +177,6 @@ export default function App() {
         let key = Object.keys(data);
         return (
           <div key={index}>
-            {/* <Checkbox.Group
-              options={key.slice(1)}
-              value={data.value}
-              onChange={(value) => onChange(value, index)}
-            /> */}
-
             {mbutton(key.slice(1), data.value, data, (value) =>
               onChange(value, index)
             )}
